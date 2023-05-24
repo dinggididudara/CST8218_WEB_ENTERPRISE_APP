962ddd65c51e84da2e45670a6a03e8b48b61b3a4
lee00665 <dinggididudara@gmail.com>
2023-05-24 14:06:20 -0400

    Lab3: no response from postman

---
+++ mavenproject3/src/main/java/cst8218/lee00665/mavenproject3/resources/JavaEE8Resource.java
@@ -0,0 +1,20 @@
+package cst8218.lee00665.mavenproject3.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("javaee8")
+public class JavaEE8Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping")
+                .build();
+    }
+}
