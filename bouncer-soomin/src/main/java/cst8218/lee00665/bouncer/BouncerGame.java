5d4b8b7a98b3c518a90429e6e1892df5cc6ecac6
dinggididudara <dinggididudara@gmail.com>
2023-05-31 01:25:13 -0400

    Assignment 1 init

---
+++ bouncer-soomin/src/main/java/cst8218/lee00665/bouncer/BouncerGame.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB31/SingletonEjbClass.java to edit this template
+ */
+package cst8218.lee00665.bouncer;
+
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+/**
+ *
+ * @author lee00665
+ */
+@Startup
+@Singleton
+public class BouncerGame {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
---
+++ bouncer-soomin/src/main/java/cst8218/lee00665/bouncer/presentation/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package cst8218.lee00665.bouncer.presentation;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
